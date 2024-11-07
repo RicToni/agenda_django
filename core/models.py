e84daaa2bbcd54971dc8e96f56c552448df80f99
Ricardo Tonholli <ricardotonholli@gmail.com>
2024-11-07 00:40:11 -0300

    class Evento criada no Models!

--- core/models.py
+++ core/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Evento(models.Model):
+    título = models.CharField(max_length=100)
+    descricao = models.TextField(blank=True, null=True)
+    data_evento = models.DateTimeField()
+    data_criacao = models.DateTimeField(auto_now=True)
+
+    
