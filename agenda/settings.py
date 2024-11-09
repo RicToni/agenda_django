0dd5c0a26d290a267d149b7ac9d3224b619da69d
Ricardo Tonholli <ricardotonholli@gmail.com>
2024-11-08 21:46:21 -0300

    views and templates

--- agenda/settings.py
+++ agenda/settings.py
@@ -1,3 +1,5 @@
+import os
+
 """
 Django settings for agenda project.
 
@@ -55,7 +57,7 @@ ROOT_URLCONF = 'agenda.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
