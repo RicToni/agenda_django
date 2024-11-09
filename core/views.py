509b825576cd92bd0583fec58a012835107942a8
Ricardo Tonholli <ricardotonholli@gmail.com>
2024-11-09 01:13:55 -0300

    views update

--- core/views.py
+++ core/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from core.models import Evento
 
 # Create your views here.
+
+def index(request):
+    return redirect('/agenda/')
+
 def lista_eventos(request):
     usuario = request.user # Define o usuário que realizou a solicitação.
     eventos = Evento.objects.filter(usuario=usuario) # Filtra dentre os eventos, apenas o evento do usuário que fez a requisição. 
