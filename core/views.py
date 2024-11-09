8f1b5ddde45d40d244652a6983a178bdab11ac68
Ricardo Tonholli <ricardotonholli@gmail.com>
2024-11-09 00:59:22 -0300

    templates update(2)

--- core/views.py
+++ core/views.py
@@ -3,6 +3,7 @@ from core.models import Evento
 
 # Create your views here.
 def lista_eventos(request):
-    eventos = Evento.objects.all()
+    usuario = request.user # Define o usuário que realizou a solicitação.
+    eventos = Evento.objects.filter(usuario=usuario) # Filtra dentre os eventos, apenas o evento do usuário que fez a requisição. 
     response = {'eventos': eventos }
     return render(request, 'agenda.html', response)
